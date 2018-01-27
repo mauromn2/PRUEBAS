b646b9d3c32b74794d555c0f236573334d001e4a
Mauro Diaz <mdiaz@spinlock.com.ar>
2018-01-27 18:29:12 -0300

    version1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Version 1");
+            Console.WriteLine("Presionar cualquier tecla para salir:");
+            Console.ReadKey();
+            Console.Beep();
         }
     }
 }
